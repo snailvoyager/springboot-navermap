9b5313016c39e62a8f912e724da4c5aa1abdee94
snailvoyager <kyj88514@gmail.com>
2023-06-01 23:17:37 +0900

    Tomcat URL parameter 특수문자 허용 처리

---
+++ src/main/java/com/snailvoyager/springbootnavermap/config/TomcatWebServerConfig.java
@@ -0,0 +1,13 @@
+package com.snailvoyager.springbootnavermap.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TomcatWebServerConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector -> connector.setProperty("relaxedQueryChars", "|{}[]"));
+    }
+}
