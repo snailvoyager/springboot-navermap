a9034477a9d14f2642b7a360ba7e22a1d1b8489a
snailvoyager <kyj88514@gmail.com>
2023-08-21 23:33:15 +0900

    단위테스트 Mock 객체 의존성 주입

--- src/test/java/com/snailvoyager/springbootnavermap/restaurant/reviewlist/service/ReviewServiceTest.java
+++ src/test/java/com/snailvoyager/springbootnavermap/restaurant/reviewlist/service/ReviewServiceTest.java
@@ -4,24 +4,27 @@ import com.snailvoyager.springbootnavermap.restaurant.reviewlist.dto.ReviewDto;
 import com.snailvoyager.springbootnavermap.restaurant.reviewlist.entity.ReviewEntity;
 import com.snailvoyager.springbootnavermap.restaurant.reviewlist.mapper.ReviewMapper;
 import com.snailvoyager.springbootnavermap.restaurant.reviewlist.repository.ReviewRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 class ReviewServiceTest {
-    @InjectMocks
     private ReviewService reviewService;
 
     @Mock
     private ReviewRepository reviewRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        reviewService = new ReviewService(reviewRepository);
+    }
 
     @Test
     void test_add() {
@@ -33,7 +36,7 @@ class ReviewServiceTest {
                         .build();
         var reviewEntity = ReviewMapper.INSTANCE.toEntity(reviewDto);
 
-        when(reviewRepository.save(reviewEntity)).thenReturn(reviewEntity);
+        when(reviewRepository.save(any())).thenReturn(reviewEntity);
         //when
         var saveDto = reviewService.add(reviewDto);
         //then
